c6b164704597bb1c85ecaec3a739bbd8cc705a9f
Andre Rodriguez <andre.rodriguez9722@outlook.com>
2023-11-13 19:39:18 -0500

    Initial commit

---
+++ test/flask-ecs-redis.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as FlaskEcsRedis from '../lib/flask-ecs-redis-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/flask-ecs-redis-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new FlaskEcsRedis.FlaskEcsRedisStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
