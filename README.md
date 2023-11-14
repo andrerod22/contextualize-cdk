2ee37b60835942a8102dc776a5ccb13a16a8ec10
Andre Rodriguez <andre.rodriguez9722@outlook.com>
2023-11-13 21:29:50 -0500

    Contextualize LLC Assignment

--- README.md
+++ README.md
@@ -12,3 +12,80 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `cdk deploy`      deploy this stack to your default AWS account/region
 * `cdk diff`        compare deployed stack with current state
 * `cdk synth`       emits the synthesized CloudFormation template
+
+
+# Flask-ECS-Redis Application
+
+This project deploys a Flask application with Nginx and Redis on AWS ECS using AWS CDK.
+
+## Prerequisites
+
+- AWS CLI
+- Docker
+- Node.js and npm
+- TypeScript
+- AWS CDK
+
+## Setup
+
+### Install TypeScript
+
+TypeScript is required for AWS CDK. Install it globally using npm:
+
+```bash
+npm install -g typescript
+
+# Install Docker
+
+Docker is used for creating containerized applications. Follow the official guide to install Docker: Docker Installation
+AWS Setup
+
+Ensure you have configured AWS CLI with the appropriate credentials and region.
+Deployment
+
+To deploy the application, run the following commands:
+
+bash
+
+cdk bootstrap
+cdk deploy
+
+After successful deployment, the application will be accessible via the URL provided by the load balancer in your AWS ECS service.
+Accessing the Application
+
+To access the Flask web application, use the URL of the load balancer found in the AWS ECS service details in the AWS Management Console.
+Additional Information
+
+For more detailed instructions and troubleshooting, refer to the official AWS documentation.
+
+javascript
+
+
+### `requirements.txt`
+
+Place this file in the `flask-app` folder within the `resources` directory. Here's an example `requirements.txt` for a basic Flask application:
+
+Flask==2.0.1
+redis==3.5.3
+
+csharp
+
+
+### Project Structure
+
+Your project directory should look something like this:
+
+.
+├── README.md
+├── bin
+│ └── ...
+├── lib
+│ └── ...
+├── resources
+│ ├── flask-app
+│ │ ├── Dockerfile
+│ │ ├── app.py
+│ │ └── requirements.txt
+│ └── nginx
+│ └── ...
+└── ...
--- lib/flask-ecs-redis-stack.ts
+++ lib/flask-ecs-redis-stack.ts
@@ -1,16 +1,68 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from '@aws-cdk/core';
+import * as ecs from '@aws-cdk/aws-ecs';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as elasticache from '@aws-cdk/aws-elasticache';
+import * as ecs_patterns from '@aws-cdk/aws-ecs-patterns';
 
 export class FlaskEcsRedisStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Define the VPC
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 3 });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'FlaskEcsRedisQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Define the ECS Cluster
+    const cluster = new ecs.Cluster(this, 'MyCluster', {
+      vpc: vpc
+    });
+
+    // Define the ECS Task Definition for Flask App and Nginx
+    const appTaskDef = new ecs.FargateTaskDefinition(this, 'AppTaskDef', {
+      memoryLimitMiB: 512, // Adjust based on your application's needs
+      cpu: 256,
+    });
+
+    // Add Flask App Container to Task Definition
+    const flaskContainer = appTaskDef.addContainer('FlaskAppContainer', {
+      image: ecs.ContainerImage.fromRegistry('897946215384.dkr.ecr.us-east-1.amazonaws.com/flask-app-repo:latest'),
+      environment: { // Simple environment variables
+        'FLASK_ENV': 'development'
+      },
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'FlaskApp' }),
+    });
+
+    // Port mapping for Flask App
+    flaskContainer.addPortMappings({
+      containerPort: 5000, // Flask default port
+    });
+
+    // Add Nginx Container to Task Definition
+    const nginxContainer = appTaskDef.addContainer('NginxContainer', {
+      image: ecs.ContainerImage.fromRegistry('897946215384.dkr.ecr.us-east-1.amazonaws.com/nginx-repo:latest'),
+      logging: ecs.LogDrivers.awsLogs({ streamPrefix: 'Nginx' }),
+    });
+
+    // Port mapping for Nginx
+    nginxContainer.addPortMappings({
+      containerPort: 80, // Nginx default port
+    });
+
+    // Define the ECS Service using an ECS pattern
+    const appService = new ecs_patterns.ApplicationLoadBalancedFargateService(this, 'AppService', {
+      cluster: cluster,
+      taskDefinition: appTaskDef,
+      publicLoadBalancer: true, // Set to true to allow public access
+    });
+
+    // Define the Elasticache Redis Cache
+    const redisCache = new elasticache.CfnCacheCluster(this, 'RedisCache', {
+      cacheNodeType: 'cache.t2.micro',
+      engine: 'redis',
+      numCacheNodes: 1,
+      vpcSecurityGroupIds: ['sg-0f51d4510266b4af4'], // Replace with your VPC security group ID
+      cacheSubnetGroupName: 'cdkflasksubnet', // Replace with your cache subnet group name
+    });
+
+    // Additional configurations can be added here
   }
 }
